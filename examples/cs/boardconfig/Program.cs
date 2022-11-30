3e11733d9eb77e981cff2d952e249c51de2f095e
rokaijano <rokaijano@gmail.com>
2022-11-30 17:10:30 +0100

    added new/old examples

---
+++ examples/cs/boardconfig/Program.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Threading;
+using mindrove;
+
+namespace boardconfig
+{
+    internal class Program
+    {
+        static BoardShim board_shim = null;
+        static int sampling_rate = 250;
+        static int counter_idx = 0;
+        static int trigger_idx = 0;
+        static int n_package = 0;
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            try
+            {
+                BoardShim.enable_dev_board_logger();
+                MindRoveInputParams input_params = new MindRoveInputParams();
+                int board_id = (int)BoardIds.MINDROVE_WIFI_BOARD;
+                board_shim = new BoardShim(board_id, input_params);
+                board_shim.prepare_session();
+                board_shim.start_stream();
+
+                sampling_rate = BoardShim.get_sampling_rate(board_id);
+                counter_idx = BoardShim.get_package_num_channel(board_id);
+                trigger_idx = BoardShim.get_other_channels(board_id)[0];
+                Console.WriteLine("Device ready");
+            }
+            catch (MindRoveException ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            if (board_shim != null)
+            {
+                // Enable EEG mode and read 5 seconds of EEG data
+                board_shim.config_board(BoardShim.MindroveWifiConfigMode.EEG_MODE);
+                Console.WriteLine("Device configured in EEG mode");
+                Console.WriteLine("Read 5 seconds of EEG data");
+                
+                int n = 0;
+                while (n < 5 * sampling_rate)
+                {
+                    double[,] data = board_shim.get_current_board_data(2);
+                    if (data[counter_idx, 0] > n_package)
+                    {
+                        string str = "";
+                        for (int i = 0; i < data.GetLength(1); i++)
+                        {
+                            for (int j = 0; j < data.GetLength(0); j++)
+                                str = string.Concat(str, " ", data[j, i]);
+                            str = string.Concat(str, "\n");
+                        }
+                        n_package = (int)data[counter_idx, 1];
+                        Console.WriteLine(str);
+                        n = n + 2;
+                    }
+                }
+
+                // Enable impedance mode and read 5 seconds of data
+                board_shim.config_board(BoardShim.MindroveWifiConfigMode.IMP_MODE);
+                Console.WriteLine("Device configured in impedance mode");
+                Console.WriteLine("Read 5 seconds of impedance data");
+
+                n = 0;
+                while (n < 5 * sampling_rate)
+                {
+                    double[,] data = board_shim.get_current_board_data(2);
+                    if (data[counter_idx, 0] > n_package)
+                    {
+                        string str = "";
+                        for (int i = 0; i < data.GetLength(1); i++)
+                        {
+                            for (int j = 0; j < data.GetLength(0); j++)
+                                str = string.Concat(str, " ", data[j, i]);
+                            str = string.Concat(str, "\n");
+                        }
+                        n_package = (int)data[counter_idx, 1];
+                        Console.WriteLine(str);
+                        n = n + 2;
+                    }
+                }
+
+                // Enable EEG mode again and read 5 seconds of test signal
+                board_shim.config_board(BoardShim.MindroveWifiConfigMode.EEG_MODE);
+                board_shim.config_board(BoardShim.MindroveWifiConfigMode.TEST_SIGNAL);
+                Console.WriteLine("Device configured in EEG mode");
+                Console.WriteLine("Read 5 seconds of test signal");
+
+                n = 0;
+                while (n < 5 * sampling_rate)
+                {
+                    double[,] data = board_shim.get_current_board_data(2);
+                    if (data[counter_idx, 0] > n_package)
+                    {
+                        string str = "";
+                        for (int i = 0; i < data.GetLength(1); i++)
+                        {
+                            for (int j = 0; j < data.GetLength(0); j++)
+                                str = string.Concat(str, " ", data[j, i]);
+                            str = string.Concat(str, "\n");
+                        }
+                        n_package = (int)data[counter_idx, 1];
+                        Console.WriteLine(str);
+                        n = n + 2;
+                    }
+                }
+
+                // Switch back to EEG measurement mode, send and receive trigger signals
+                board_shim.config_board(BoardShim.MindroveWifiConfigMode.EEG_MODE);
+                Console.WriteLine("Device configured in EEG mode");
+                Thread.Sleep(100);  // Wait a little after switching modes
+
+                board_shim.config_board(BoardShim.MindroveWifiConfigMode.BEEP);
+                Console.WriteLine("BEEP sent");
+
+                board_shim.config_board(BoardShim.MindroveWifiConfigMode.BOOP);
+                Console.WriteLine("BOOP sent");
+
+                bool beep_received = false;
+                bool boop_received = false;
+                while (!(beep_received && boop_received))
+                {
+                    double[,] data = board_shim.get_current_board_data(2);
+                    if (data[counter_idx, 0] > n_package)
+                    {
+                        for (int i = 0; i < data.GetLength(1); i++)
+                        {
+                            if ((int)data[trigger_idx, i] == 1)
+                            {   
+                                beep_received = true;
+                                Console.WriteLine("BEEP!");
+                            }
+
+                            if ((int)data[trigger_idx, i] == 2)
+                            {
+                                boop_received = true;
+                                Console.WriteLine("BOOP!");
+                            }
+                        }
+                        n_package = (int)data[counter_idx, 1];
+                    }
+                }
+
+                try
+                {
+                    board_shim.release_session();
+                }
+                catch (MindRoveException e)
+                {
+                    Console.WriteLine(e);
+                }
+                Console.WriteLine("Brainflow streaming was stopped");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ examples/python/boardconfig/boardconfig.py
@@ -0,0 +1,122 @@
+import time
+import mindrove
+from mindrove.board_shim import BoardShim, MindRoveInputParams, BoardIds, MindRoveError
+
+try:
+    BoardShim.enable_dev_board_logger()
+
+    params = MindRoveInputParams()
+    params.ip_port = 0
+    params.serial_port = ''
+    params.mac_address = ''
+    params.other_info = ''
+    params.serial_number = ''
+    params.ip_address = ''
+    params.ip_protocol = 0
+    params.timeout = 0
+    params.file = ''
+
+    board_id = BoardIds.MINDROVE_WIFI_BOARD.value
+    board_shim = BoardShim(board_id, params)
+    board_shim.prepare_session()
+    board_shim.start_stream(450000)
+
+    sampling_rate = board_shim.get_sampling_rate(board_id)
+    counter_idx = board_shim.get_package_num_channel(board_id)
+    trigger_idx = board_shim.get_other_channels(board_id)[0]
+    n_package = 0
+    print("Device ready")
+except MindRoveError:
+    print(MindRoveError)
+
+if board_shim != None:
+    # Enable EEG mode and read 5 seconds of EEG data
+    board_shim.config_board(mindrove.MindroveConfigMode.EEG_MODE)
+    print("Device configured in EEG mode")
+    print("Read 5 seconds of EEG data")
+
+    n = 0
+    while n < 5 * sampling_rate:
+        data = board_shim.get_current_board_data(2)
+        if data.shape[1] < 2:
+            continue
+        if data[counter_idx, 0] > n_package:
+            s = ""
+            for i in range(data.shape[1]):
+                for j in range(data.shape[0]):
+                    s = s + " " + str(data[j, i])
+                s = s + "\n"
+        n_package = data[counter_idx, 1]
+        print(s)
+        n = n + 2
+
+    # Enable impedance mode and read 5 seconds of data
+    board_shim.config_board(mindrove.MindroveConfigMode.IMP_MODE)
+    print("Device configured in impedance mode")
+    print("Read 5 seconds of impedance data")
+
+    n = 0
+    while n < 5 * sampling_rate:
+        data = board_shim.get_current_board_data(2)
+        if data.shape[1] < 2:
+            continue
+        if data[counter_idx, 0] > n_package:
+            s = ""
+            for i in range(data.shape[1]):
+                for j in range(data.shape[0]):
+                    s = s + " " + str(data[j, i])
+                s = s + "\n"
+        n_package = data[counter_idx, 1]
+        print(s)
+        n = n + 2
+
+    # Enable EEG mode again and read 5 seconds of test signal
+    board_shim.config_board(mindrove.MindroveConfigMode.EEG_MODE)
+    board_shim.config_board(mindrove.MindroveConfigMode.TEST_SIGNAL)
+    print("Device configured in EEG mode")
+    print("Read 5 seconds of test signal")
+
+    n = 0
+    while n < 5 * sampling_rate:
+        data = board_shim.get_current_board_data(2)
+        if data.shape[1] < 2:
+            continue
+        if data[counter_idx, 0] > n_package:
+            s = ""
+            for i in range(data.shape[1]):
+                for j in range(data.shape[0]):
+                    s = s + " " + str(data[j, i])
+                s = s + "\n"
+        n_package = data[counter_idx, 1]
+        print(s)
+        n = n + 2
+
+    # Switch back to EEG measurement mode, send and receive trigger signals
+    board_shim.config_board(mindrove.MindroveConfigMode.EEG_MODE)
+    print("Device configured in EEG mode")
+    time.sleep(0.1) # Wait a little after switching modes
+
+    board_shim.config_board(mindrove.MindroveConfigMode.BEEP)
+    print("BEEP sent")
+
+    board_shim.config_board(mindrove.MindroveConfigMode.BOOP)
+    print("BOOP sent")
+
+    beep_received = False
+    boop_received = False
+    while not(beep_received and boop_received):
+        data = board_shim.get_current_board_data(2)
+        if data[counter_idx, 0] > n_package:
+            for i in range(data.shape[1]):
+                if data[trigger_idx, i] == 1:
+                    beep_received = True
+                    print("BEEP!")
+                if data[trigger_idx, i] == 2:
+                    boop_received = True
+                    print("BOOP!")
+        n_package = data[counter_idx, 1]
+    try:
+        board_shim.release_session()
+    except MindRoveError:
+        print(MindRoveError)
+    print("Brainflow streaming was stopped")
